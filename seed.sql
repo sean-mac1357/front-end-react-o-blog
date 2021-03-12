6790fcf4ff1d882b92f7a2a88c70e5471357fac3
Sean R MacEachern <sean.r.maceachern@gmail.com>
2021-03-12 16:33:58 -0500

    changed images in seed.sql

--- seed.sql
+++ seed.sql
@@ -1,7 +1,7 @@
 INSERT INTO react_o_blog (name, slug, attribute, image) VALUES 
-('Rainbow Dash', 'rainbow_dash', 'can allow her to fly extremely fast', 'https://i.pinimg.com/originals/4f/b0/43/4fb043bdb26de5a423eb2f2175f36d31.png'),
-('Flutter Shy', 'flutter_shy', 'can allow her to sway animals to help her complete tasks', 'https://i.pinimg.com/originals/07/70/e2/0770e29b1a312b4577002acc62c01535.png'),
-('Pinkie Pie', 'pinkie_pie', 'can allow her to make cupcakes and is extremely sweet', 'https://lh3.googleusercontent.com/proxy/Z3tG8SyDPJQQTvFtzWY73xFcOfoLl_Iq5LMSOfld1MasHjG36Davd43DcTwOJsMJOQP98XGSbwwVvSRB5vxKnU0rj6H_EedoYS5zgoNM_dFgkV3x9YiLTJSSGmzFquXJI26Fq5TU_At_fQddGrtR'),
-('Twilight Sparkle', 'twilight_sparkle', 'can allow her to use telekinesis to make stuff float', 'https://i.pinimg.com/originals/49/b4/1a/49b41a9b3a0a30a0501b4079c424042b.png'),
-('Apple Jack', 'apple_jack', 'can allow her to use her strength and courageousness to lead the group', 'https://i.pinimg.com/originals/fe/b8/ba/feb8ba624d135da99704a256d0900f8f.png'),
-('Rarity', 'rarity', 'can allow her to make fashionable clothes', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtnU_2wqzYywFLvkzVJ9xfF2kHwSg7pGzkcg&usqp=CAU')
+('Rainbow Dash', 'rainbow_dash', 'can allow her to fly extremely fast', 'https://i.pinimg.com/originals/c3/f7/7a/c3f77af44e33c7f1825533b0c7eb299b.png'),
+('Flutter Shy', 'flutter_shy', 'can allow her to sway animals to help her complete tasks', 'https://i.ebayimg.com/images/g/K6UAAOSwnipWUQop/s-l300.jpg'),
+('Pinkie Pie', 'pinkie_pie', 'can allow her to make cupcakes and is extremely sweet', 'https://lh3.googleusercontent.com/proxy/ym2Mv5t6ZoJmSwpYSryTIHIxgncr94vUZt0Q2ANjbktB0vN-rMbYojo8-2K1glS7kjxIZUQrUErYcwASkCCEzXie6EK-GA75g7tnxGWQuo2XIEF6Bwx4bGZ9jFP7yVeAI3MG08YjKyBIJjxwQ2J9'),
+('Twilight Sparkle', 'twilight_sparkle', 'can allow her to use telekinesis to make stuff float', 'https://i.ebayimg.com/images/g/kt0AAOSwT4lWTfdj/s-l300.jpg'),
+('Apple Jack', 'apple_jack', 'can allow her to use her strength and courageousness to lead the group', 'https://sites.google.com/site/mylittleponyfansonly/_/rsrc/1445221559312/applejack/Apple5.png'),
+('Rarity', 'rarity', 'can allow her to make fashionable clothes', 'https://i.pinimg.com/236x/45/61/e7/4561e715f838eff33d2a9eabc3ac8013.jpg')
